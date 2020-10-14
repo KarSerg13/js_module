5b7b8231655af73450837c9c19b9c6ce92877cfe
Sergii Karastoyanov <karastoyanov13@gmail.com>
2020-10-14 19:52:20 +0300

    test

---
+++ demo3/main.js
@@ -0,0 +1,22 @@
+// let num = 10;
+//
+// if (num %2 == 0) {
+//     console.log('even');
+//     }else { console.log('odd')}
+
+
+
+// let  color = ',jg,v,jb';
+// if (color == 'green') {
+//     console.log('go!');
+// }else if (color == 'yellow'){
+//     console.log('wait');
+// }else if (color == 'red') {
+//     console.log('stop');
+// }else {console.log('defoult');
+// }
+let num = 68;
+
+if (num % 2 == 0 || num % 3 == 0){
+    console.log('this number is multiple  2 or 3')
+}
